fd7d307cc5b5d81ff0375bf826070388c00de0df
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2024-01-21 22:04:41 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "04usePopcorn/",
+  base: "/04usePopcorn/",
   plugins: [react()],
 })
