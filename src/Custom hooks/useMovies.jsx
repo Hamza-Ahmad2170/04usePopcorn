8524b5becb859cca56c28b467019bb53395c72f8
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2024-01-21 22:16:49 +0500

    Update useMovies.jsx

--- src/Custom hooks/useMovies.jsx
+++ src/Custom hooks/useMovies.jsx
@@ -21,7 +21,7 @@ function useMovies(query) {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
           {
             signal: controller.signal,
           }
